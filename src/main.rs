23082cd79cfcf42916b1ca92ed0b546d3360deea
sokorototo <nyachiengatoto@gmail.com>
2025-07-03 17:43:06 +0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -101,6 +101,6 @@ async fn main() {
 	});
 
 	// solve
-	solver::solve::<{ solver::HASHES_READ_FLAG }>(&config, &device, &queue, sender);
+	solver::solve::<{ solver::MATCHES_READ_FLAG | solver::HASHES_READ_FLAG }>(&config, &device, &queue, sender);
 	let _result = handle.join().expect("Monitoring thread experienced an error");
 }
